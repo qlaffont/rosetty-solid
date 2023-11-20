84815220c94b4d3ea328f082fa18c43a34f5afde
Quentin <qlaffont13@gmail.com>
2023-11-20 08:29:56 +0100

    fix(app): fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -35,18 +35,5 @@ export default defineConfig((config) => {
 
   const parsed_data = preset.parsePresetOptions(preset_options, watching);
 
-  if (!watching) {
-    const package_fields = preset.generatePackageExports(parsed_data);
-
-    console.log(
-      `\npackage.json: \n${JSON.stringify(package_fields, null, 2)}\n\n`
-    );
-
-    /*
-        will update ./package.json with the correct export fields
-    */
-    preset.writePackageJson(package_fields);
-  }
-
   return preset.generateTsupOptions(parsed_data);
 });
